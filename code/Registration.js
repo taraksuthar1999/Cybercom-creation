e926aca97fa3f4ceb68bdfccd8007961becf6d68
tarak suthar <tarak.suthar1999@gmail.com>
2021-01-21 11:17:16 +0530

    added initial files

---
+++ code/Registration.js
@@ -0,0 +1,19 @@
+var admin = [];
+function checkAdmin() {
+    if (localStorage.getItem('admin')) {
+        alert("Already Registered..");
+        window.location.href = "Login.html";
+    }
+}
+
+function registerAdmin() {
+    var adminObj = {};
+    adminObj.name = document.getElementById("name").value;
+    adminObj.email = document.getElementById("email").value;
+    adminObj.password = document.getElementById("password").value;
+    adminObj.city = document.getElementById("selectCity").value;
+    adminObj.state = document.getElementById("selectState").value;
+    admin.push(adminObj);
+    localStorage.setItem("admin", JSON.stringify(admin));
+    window.location.href = "Login.html";
+}
---
+++ code/login.js
@@ -0,0 +1,4 @@
+function gotoRegistration() {
+    console.log("lol");
+    window.location.href = "Registration.html";
+}
